a0b8746ccef11b3e1b339c4d0ec75dbb0202cd30
Vatsal <i-vatsal.patel@devrev.ai>
2024-11-07 09:47:42 +0530

    react router added

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,38 @@
+import { BrowserRouter, Link, Routes, Route } from 'react-router-dom'
 import './App.css'
 import MainComponent from './components/MainComponent/MainComponent'
-import SuperBoardComponent from './components/SuperBoardComponent/SuperBoardComponent'
 import SuperComponent from './components/SuperComponent/SuperComponent'
 import TitleComponent from './components/TitleComponent/TitleComponent'
 
 function App() {
 
-  return (
-    <>
-      <TitleComponent />
-      <SuperComponent />
-      {/* <MainComponent /> */}
-    </>
-  )
+    return (
+        // <>
+        //   <TitleComponent />
+        //   <SuperComponent />
+        //   {/* <MainComponent /> */}
+        // </>
+        <BrowserRouter>
+            <TitleComponent />
+            <ul id='navBar'> 
+                <li>
+                    <Link to='/' className='link'>Home</Link>
+                </li>
+                <li>
+                    <Link to='/super' className='link'>Super</Link>
+                </li>
+                {/* <li>
+                    <Link to='/main'>Main</Link>
+                </li> */}
+            </ul>
+
+            <Routes>
+                <Route path='/' element={<MainComponent />} />
+                <Route path='/super' element={<SuperComponent />} />
+                {/* <Route path='/main' element={<MainComponent />} /> */}
+            </Routes>
+        </BrowserRouter>
+    )
 }
 
 export default App
